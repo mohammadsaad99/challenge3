fc61f35cab05a761ce02987d23828d9e734215c8
mohammadsaad99 <103560774+mohammadsaad99@users.noreply.github.com>
2022-04-15 00:50:53 +0300

    eleventh commit

--- GetRating.cs
+++ GetRating.cs
@@ -24,7 +24,7 @@ namespace Company.Function
             }                                 
             else
             {                 
-                return new OkObjectResult(rating);                 
+                return new OkObjectResult(rating);                     
             }           
         }
     }
